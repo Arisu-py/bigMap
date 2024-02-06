8e412df64b6fc23fb5cc6d6e4f7f14d4b5ff19b4
heigo <mstal64@yandex.ru>
2024-02-06 17:16:04 +0400

    part1

---
+++ first.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.getImage()
+        self.initUI()
+
+    def get_coords(self, text):
+        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={text}&format=json"
+
+        # Выполняем запрос.
+        response = requests.get(geocoder_request)
+        if response:
+            # Преобразуем ответ в json-объект
+            json_response = response.json()
+
+            # Получаем первый топоним из ответа геокодера.
+            # Согласно описанию ответа, он находится по следующему пути:
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
+                "GeoObject"]
+            # Полный адрес топонима:
+            # Координаты центра топонима:
+            toponym_coodrinates = toponym["Point"]["pos"]
+            return ','.join(toponym_coodrinates.split())
+            # Печатаем извлечённые из ответа поля:
+        else:
+            print("Ошибка выполнения запроса:")
+            print(geocoder_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+
+    def getImage(self):
+        coords = '46.034077,51.529814'
+        spn = '0.000457,0.00419'
+        map_request = f"https://static-maps.yandex.ru/1.x/?ll={coords}&spn={spn}&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
